5c15bee7ebf5d98cc7250b4d77e8e8b9d8dc0470
henzo gomes <henzo.gomes@newm.com.br>
2021-04-24 20:11:06 -0300

    Aula 3

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 
+import colors from '../styles/colors'
+
 import { Welcome } from '../pages/Welcome'
 import { UserIdentification } from '../pages/UserIdentification'
 import { Confirmation } from '../pages/Confirmation'
-
-import colors from '../styles/colors'
+import { PlantSelect } from '../pages/PlantSelect'
 
 const stackRoutes = createStackNavigator()
 
@@ -32,6 +33,11 @@ const AppRoutes: React.FC = () => (
         <stackRoutes.Screen
             name="Confirmation"
             component={Confirmation}
+        />
+
+        <stackRoutes.Screen
+            name="PlantSelect"
+            component={PlantSelect}
         />
 
     </stackRoutes.Navigator>
